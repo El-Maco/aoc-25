467651419f6dcf0a7e9036302031d3391c7d23fa
Marcus Myllyviita <marcus.myllyviita@afry.com>
2025-12-09 10:31:52 +0200

    day09 part2 incomplete

--- src/bin/day09.rs
+++ src/bin/day09.rs
@@ -27,8 +27,106 @@ fn solve_part1(input: &str) -> u64 {
     largest_area
 }
 
+fn display_grid(grid: &Vec<String>) {
+    for line in grid {
+        println!("{}", line);
+    }
+}
+
 fn solve_part2(input: &str) -> u64 {
-    0
+    let rows = input
+        .lines()
+        .map(|r| {
+            let mut split = r.split(',');
+            let x: usize = split.next().unwrap().parse().unwrap();
+            let y: usize = split.next().unwrap().parse().unwrap();
+            (x, y)
+        })
+        .collect::<Vec<(usize, usize)>>();
+
+    println!("Rows parsed");
+
+    let max_x = rows.iter().map(|(x, _)| *x).max().unwrap();
+    let max_y = rows.iter().map(|(_, y)| *y).max().unwrap();
+    let mut str_grid: Vec<String> = vec![".".repeat(max_x + 1); max_y + 1];
+    println!("Grid initialized");
+    for row in &rows {
+        str_grid[row.1].replace_range(row.0..=row.0, "#");
+    }
+
+    println!("Red tiles set");
+    rows.iter().for_each(|&(x, y)| {
+        // Set green tiles right until the next red tile
+        let last_red_tile_on_row = rows
+            .iter()
+            .filter(|&&(_, yy)| yy == y)
+            .map(|&(xx, _)| xx).max()
+            .unwrap();
+
+        for xx in x+1..last_red_tile_on_row {
+            if str_grid[y].as_bytes()[xx] == b'#' {
+                continue;
+            }
+            str_grid[y].replace_range(xx..=xx, "G");
+        }
+
+        let last_red_tile_on_column = rows
+            .iter()
+            .filter(|&&(xx, _)| xx == x)
+            .map(|&(_, yy)| yy).max()
+            .unwrap();
+
+        for yy in y+1..last_red_tile_on_column {
+            if str_grid[yy].as_bytes()[x] == b'#' {
+                continue;
+            }
+            str_grid[yy].replace_range(x..=x, "G");
+        }
+    });
+
+    println!("Green tiles set");
+
+    str_grid.iter_mut().enumerate().for_each(|(i, line)| {
+        println!("Filling row {}/{}", i, max_y);
+        let first_g_or_red = line.find(|c| c == 'G' || c == '#');
+        let last_g_or_red = line.rfind(|c| c == 'G' || c == '#');
+        if let (Some(first), Some(last)) = (first_g_or_red, last_g_or_red) {
+            for i in first..=last {
+                if line.as_bytes()[i] == b'.' {
+                    line.replace_range(i..=i, "G");
+                }
+            }
+        }
+    });
+
+    println!("Grid done");
+
+    display_grid(&str_grid);
+
+    let mut largest_area = 0;
+    rows.iter().for_each(|&(first_x, first_y)| {
+        rows.iter().for_each(|&(second_x, second_y)| {
+            if second_x > first_x && second_y > first_y {
+                let is_in_area = str_grid[first_y..second_y]
+                    .iter()
+                    .map(|row| {println!("row: {:?}", row); &row[first_x..second_x]})
+                    .all(|slice| slice.chars().all(|c| c == 'G' || c == '#'));
+
+                if !is_in_area {
+                    return;
+                }
+
+                let area = (((first_x as isize - second_x as isize).abs() + 1)
+                    * ((first_y as isize - second_y as isize).abs() + 1))
+                    as u64;
+                if area > largest_area {
+                    println!("New largest area found: {} (from ({},{}) to ({},{}))", area, first_x, first_y, second_x, second_y);
+                    largest_area = area;
+                }
+            }
+        });
+    });
+    largest_area
 }
 
 fn main() {
@@ -39,6 +137,16 @@ fn main() {
     let part2 = solve_part2(&input_text);
     println!("Part 2: The result is {}", part2);
 }
+
+// ..............
+// .......#...#..
+// ..............
+// ..#....#......
+// ..............
+// ..#......#....
+// ..............
+// .........#.#..
+// ..............
 
 #[cfg(test)]
 mod tests {
@@ -61,6 +169,6 @@ mod tests {
     #[test]
     fn test_solve_part2() {
         let result = solve_part2(TEST_INPUT);
-        assert_eq!(result, 0);
+        assert_eq!(result, 24);
     }
 }
